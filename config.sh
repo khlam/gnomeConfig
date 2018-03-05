d28110d67d087fb1a4e3eabdd41dcc6671e64de7
khlam <4841220+khlam@users.noreply.github.com>
2018-03-05 14:51:56 -0800

    removes all ubuntu gnome bloat

--- config.sh
+++ config.sh
@@ -7,4 +7,11 @@ cp -v /etc/xdg/autostart/tracker-* ./
 for FILE in `ls`; do echo Hidden=true >> $FILE; done
 rm -rf ~/.cache/tracker ~/.local/share/tracker
 
-sudo apt purge rhythmbox gnome-contacts gnome-mahjongg gnome-mines gnome-sudoku gnome-music gnome-maps rhythmbox empathy
+sudo apt purge rhythmbox gnome-contacts gnome-mahjongg gnome-mines gnome-sudoku gnome-music gnome-maps gnome-photos gnome-weather gnome-documents rhythmbox empathy evolution aisleriot brasero* simple-scan gnome-calendar cheese*
+
+# Remove Libreoffice
+sudo apt-get remove --purge libreoffice*
+
+# Final Clean
+sudo apt-get clean
+sudo apt-get autoremove
