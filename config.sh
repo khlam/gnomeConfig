6285d05c9645cb22bd92eb7c57482187c92f72bc
khlam <4841220+khlam@users.noreply.github.com>
2018-03-15 10:42:04 -0700

    added PPAs and packages I use

--- config.sh
+++ config.sh
@@ -7,10 +7,23 @@ cp -v /etc/xdg/autostart/tracker-* ./
 for FILE in `ls`; do echo Hidden=true >> $FILE; done
 rm -rf ~/.cache/tracker ~/.local/share/tracker
 
+# Purge everything else
 sudo apt purge rhythmbox gnome-contacts gnome-mahjongg gnome-mines gnome-sudoku gnome-music gnome-maps gnome-photos gnome-weather gnome-documents rhythmbox empathy evolution aisleriot brasero* gedit simple-scan gnome-calendar
 
-# Disable Bluetooth
-sudo echo "manual" > /etc/init/bluetooth.override
+# Get TLP PPA
+sudo add-apt-repository ppa:linrunner/tlp -y
+
+# Get KeepassXC PPA
+sudo add-apt-repository ppa:phoerious/keepassxc -y
+
+# Update && Upgrade
+sudo apt upgrade && sudo apt upgrade
+
+# Install safe-rm
+sudo apt install safe-rm
+
+# Install KeepassXC
+sudo apt install keepassxc 
 
 # Remove Libreoffice
 sudo apt-get remove --purge libreoffice*
