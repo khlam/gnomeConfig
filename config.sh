99a52974a54b7b7669da681a52b0e81b19723e81
khlam <4841220+khlam@users.noreply.github.com>
2019-06-22 14:00:49 -0700

    Xubuntu keyboard shortcuts

--- config.sh
+++ config.sh
@@ -1,14 +1,17 @@
 # Config for dell xps 13
 
-# Disable Tracker
+# Disable Trackers
 tracker daemon -t
 cd ~/.config/autostart
 cp -v /etc/xdg/autostart/tracker-* ./
 for FILE in `ls`; do echo Hidden=true >> $FILE; done
 rm -rf ~/.cache/tracker ~/.local/share/tracker
 
+# Move xfce keyboard shortcuts
+mv xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
 # Purge everything else
-sudo apt purge rhythmbox gnome-contacts gnome-mahjongg gnome-mines gnome-sudoku gnome-music gnome-maps gnome-photos gnome-weather gnome-documents rhythmbox empathy evolution aisleriot brasero* gedit simple-scan gnome-calendar
+sudo apt purge rhythmbox gnome-contacts gnome-mahjongg gnome-mines gnome-sudoku gnome-music gnome-maps gnome-photos gnome-weather gnome-documents rhythmbox empathy evolution aisleriot brasero* gedit simple-scan gnome-calendar thunderbird
 
 # TLP PPA
 sudo add-apt-repository ppa:linrunner/tlp -y
@@ -16,13 +19,10 @@ sudo add-apt-repository ppa:linrunner/tlp -y
 # KeepassXC PPA
 sudo add-apt-repository ppa:phoerious/keepassxc -y
 
-# Libreoffice PPA
-sudo add-apt-repository ppa:libreoffice/ppa
-
 sudo apt update 
 sudo apt upgrade
 
-sudo apt install keepassxc tlp libreoffice vim
+sudo apt install keepassxc tlp vim
 
 # done
 sudo apt-get clean
